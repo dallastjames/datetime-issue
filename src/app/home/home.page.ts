1ce4aa5c65b14f3e965b5339e1f42aa53aab26aa
Dallas James <dallas@Dallass-MacBook-Pro.local>
2019-11-22 14:30:04 -0700

    setup ion-datetime test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  public time: string = "1990-02-19T07:43Z";
 
   constructor() {}
-
 }
